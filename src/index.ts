47363fb0391e1929e43cc7d2e1ef5c70826b02b7
Sergio Rafael Gianazza <sergio@gianu.me>
2024-05-29 19:31:02 +0000

    add logs

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,39 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
-import prClosedAction from './actions/prClosedAction';
-import prUpdatedAction from './actions/prUpdatedAction';
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import prClosedAction from "./actions/prClosedAction";
+import prUpdatedAction from "./actions/prUpdatedAction";
 
 const main = async () => {
   try {
-    const bucketName = core.getInput('bucket-prefix');
-    const folderToCopy = core.getInput('folder-to-copy');
-    const environmentPrefix = core.getInput('environment-prefix');
+    const bucketName = core.getInput("bucket-prefix");
+    const folderToCopy = core.getInput("folder-to-copy");
+    const environmentPrefix = core.getInput("environment-prefix");
 
     console.log(`Bucket Name: ${bucketName}`);
+    console.log("---- Printing env variables ----");
+    console.log(process.env);
+    console.log(JSON.stringify(process.env));
 
     const githubActionType = github.context.payload.action;
 
-    if (github.context.eventName === 'pull_request') {
+    if (github.context.eventName === "pull_request") {
       switch (githubActionType) {
-        case 'opened':
-        case 'reopened':
-        case 'synchronize':
+        case "opened":
+        case "reopened":
+        case "synchronize":
           await prUpdatedAction(bucketName, folderToCopy, environmentPrefix);
           break;
 
-        case 'closed':
+        case "closed":
           await prClosedAction(bucketName, environmentPrefix);
           break;
 
         default:
-          console.log('PR not created, modified or deleted. Skiping...');
+          console.log("PR not created, modified or deleted. Skiping...");
           break;
       }
     } else {
-      console.log('Not a PR. Skipping...');
+      console.log("Not a PR. Skipping...");
     }
   } catch (error) {
     console.log(error);
